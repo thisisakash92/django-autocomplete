e6538d07fdc50ebd9aead55c387a6c704c19507c
thisisakash92 <thisisakash92@yahoo.com>
2015-09-25 22:38:36 +0530

    Update settings.py

--- search/settings.py
+++ search/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'your_db_name',                      # Or path to database file if using sqlite3.
-        'USER': 'akash',                      # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'root',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
